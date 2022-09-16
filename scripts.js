6d3b59065ea0ce35150c519a3ae63c67df37f1db
bonvini <103939580+4545944867@users.noreply.github.com>
2022-09-16 11:26:56 -0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,21 @@
+let img = 0; //contador de imagens solicitadas
+const max = 80; //numero da ultima imagem
+const uptadeRate = 2000; //taxa de aalização automatica
+
+function proxImagem( img ){ //solicita e recebe nova imagem via fetch()  
+    fetch('img/'+img+".jpg")
+        .then(resp => resp.blob())
+        .then(blob => {
+            const imageObjectURL = URL.createObjectURL(blob); //cria endereço da imagem
+            console.log(imageObjectURL); 
+            const proxImg = document.createElement("img");
+            proxImg.src = imageObjectURL;
+            document.getElementById("placeholder").appendChild(proxImg);
+        })
+};                                                                                  
+
+window.onload = setInterval(function(){
+    proxImagem( img++ % (max + 1));
+    let  scrolloint = window.scrollY + window.innerHeight;
+    window.scrollTo({top: scrollPoint, behavior: 'smooth'});
+}, uptadeRate);
